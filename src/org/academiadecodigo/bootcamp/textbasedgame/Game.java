d1ae449d6114c3e7097b7009cd4df7c773ccce72
Asger <andre.12@live.com.pt>
2017-07-11 18:57:46 +0100

    more cleanups

--- src/org/academiadecodigo/bootcamp/textbasedgame/Game.java
+++ src/org/academiadecodigo/bootcamp/textbasedgame/Game.java
@@ -1,7 +1,20 @@
 package org.academiadecodigo.bootcamp.textbasedgame;
 
+import org.academiadecodigo.bootcamp.textbasedgame.view.TerminalView;
+
 /**
  * Created by codecadet on 11/07/2017.
  */
 public class Game {
+
+    private TerminalView terminalview;
+
+    public void start()(){
+        return;
+    }
+
+
+
+
+
 }
--- src/org/academiadecodigo/bootcamp/textbasedgame/view/Cell.java
+++ src/org/academiadecodigo/bootcamp/textbasedgame/view/Cell.java
@@ -4,4 +4,24 @@ package org.academiadecodigo.bootcamp.textbasedgame.view;
  * Created by codecadet on 11/07/2017.
  */
 public class Cell {
+
+    private int col;
+    private int row;
+    private char character;
+
+    public void position(int col, int row){
+        return;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public void setCharacter(char character) {
+        this.character = character;
+    }
 }
