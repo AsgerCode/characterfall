cb63a22b7109745d37ae2dfe3313a9990dfdbf9a
ritafialho <rita0fialho@gmail.com>
2017-07-16 23:55:04 +0100

    characters falling logic implemented missing ending state

--- src/org/academiadecodigo/bootcamp/textbasedgame/view/TerminalView.java
+++ src/org/academiadecodigo/bootcamp/textbasedgame/view/TerminalView.java
@@ -16,6 +16,8 @@ public class TerminalView {
     private ColorPrinter colorPrinter;
     Scanner scanner = new Scanner(System.in);
     private final int INITIAL_PLAYER_ROW = 3;
+    private final String symbolPlayerOne = "\u00A9";
+    private final String symbolPlayerTwo = "\u00A5";
 
 
     public TerminalView() throws IOException {
@@ -36,9 +38,9 @@ public class TerminalView {
 
     public void populatePlayer(int col, int row, String character) {
         if (col < (COLSIZE / 2)) {
-            col = (int) (Math.random() * ((COLSIZE - 1) / 2)) + 2;
-        } else {
-            col = (int) (Math.random() * ((COLSIZE - 1 - (COLSIZE - 1) / 2)) + (COLSIZE - 1) / 2);
+            col = (int) (Math.random() * ((COLSIZE - 4) / 2)) + 2;
+        } else {                                                       //(math.random()* MAX - MIN ) + MIN
+            col = (int) (Math.random() * ((COLSIZE - 4  - (COLSIZE - 4) / 2)) + (COLSIZE - 4) / 2);
         }
 
         fieldGrid[col][row].setCharacter(character);
@@ -60,16 +62,14 @@ public class TerminalView {
         for (int row = 0; row < ROWSIZE; row++) {
             for (int col = 0; col < COLSIZE; col++) {
 
-                if (row == INITIAL_PLAYER_ROW) {
+
                     if (fieldGrid[col][row].getCharacter().matches("\u00A9")) {
                         colorPrinter.printCharactersWithDifferentBackgroundColor(Ansi.BColor.RED, " ", " ");
                     } else if (fieldGrid[col][row].getCharacter().matches("\u00A5")) {
                         colorPrinter.printCharactersWithDifferentBackgroundColor(Ansi.BColor.BLUE, " ", " ");
-                    } else {
-                        System.out.print(fieldGrid[col][row].getCharacter());
                     }
 
-                } else {
+                 else {
                     System.out.print(fieldGrid[col][row].getCharacter());
                 }
             }
@@ -156,18 +156,19 @@ public class TerminalView {
         for (int row = 4; row < ROWSIZE - 1; row++) {
             for (int col = 2; col < COLSIZE - 2; col++) {
                 if (fieldGrid[col][row].getCharacter().equals(character)) {
-                    fieldGrid[col][row].setCharacter("");
+                    fieldGrid[col][row].setCharacter("*");
                 }
             }
         }
     }
 
     public void updateGridWithPlayerChoice() {
-        for (int row = ROWSIZE - 3; row > 4; --row) {
-            for (int col = COLSIZE - 2; col > 2; --col) {
-                if (!fieldGrid[col][row].getCharacter().equals(" ")) {
-
-                    pullCharactersDown(col, row);
+        for (int row = ROWSIZE - 3; row > 3; --row) {
+            for (int col = COLSIZE - 2; col > 1; --col) {
+                if(!fieldGrid[col][row].getCharacter().equals(" ")){
+                    if (fieldGrid[col][row].getCharacter().equals("*")) {
+                        pullCharactersDown(col, row);
+                    }
                 }
             }
         }
@@ -175,15 +176,16 @@ public class TerminalView {
 
     private void pullCharactersDown(int col, int row) {
         String character;
-        for (int i = row; i > 5; --i) {
-
+        for (int i = row; i > 3; --i) {
             character = fieldGrid[col][i - 1].getCharacter();
-
-            if (!character.equals(" ")) {
-                fieldGrid[col][i].setCharacter(character);
-            } else {
-                return;
+            if(character.matches(symbolPlayerOne)){
+                fieldGrid[col][i - 1].setCharacter(" ");
             }
+            if(character.matches(symbolPlayerTwo)){
+                fieldGrid[col][i - 1].setCharacter(" ");
+            }
+
+            fieldGrid[col][i].setCharacter(character);
         }
     }
 }
